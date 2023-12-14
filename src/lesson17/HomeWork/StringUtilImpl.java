0d39fe4dc6f1f384eb2fc42e24f54d5b0438da10
kirillfenyuk <firon13072015@gmail.com>
2023-12-14 21:35:55 +0300

    Редактирование

---
+++ src/lesson17/HomeWork/StringUtilImpl.java
@@ -0,0 +1,29 @@
+package lesson17.HomeWork;
+
+public class StringUtilImpl implements StringUtils{
+    @Override
+    public double div(String number1, String number2) throws NullPointerException, NumberFormatException, ArithmeticException {
+
+        if (number1==null||number2==null){
+            throw new NullPointerException(" number1==null||number2==null ");
+        }
+
+        int num1 = Integer.parseInt(number1);
+        int num2 = Integer.parseInt(number2);
+
+        if (num2==0){
+            throw new ArithmeticException(" num2==0 ");
+        }
+        return num1/num2;
+    }
+
+    @Override
+    public int[] findWord(String text, String word) throws NullPointerException {
+        return new int[0];
+    }
+
+    @Override
+    public double[] findNumbers(String text) throws CustomException {
+        return new double[0];
+    }
+}
