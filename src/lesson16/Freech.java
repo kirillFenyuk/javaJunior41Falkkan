0b0461f8656d702c7df656e8d9e3a209b76e5f9d
kirillfenyuk <firon13072015@gmail.com>
2023-12-17 12:38:09 +0300

    Редактирование

--- src/lesson16/Freech.java
+++ src/lesson16/Freech.java
@@ -1,6 +1,8 @@
 package lesson16;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Freech {
@@ -31,10 +33,68 @@ public class Freech {
     }
 
     // 1 взять продукт с холодильника
+    public void getProdukt(String nameOfProduct, int value) {
+
+        nameOfProduct = nameOfProduct.toLowerCase();
+
+        if (products.containsKey(nameOfProduct)) {
+            int currentValue = products.get(nameOfProduct);
+
+            if (currentValue > value) {
+                products.put(nameOfProduct, currentValue - value);
+                System.out.println(nameOfProduct + " осталось: " + (currentValue - value));
+            } else {
+                System.out.println("В холодильнике оставалось: " + currentValue);
+                System.out.println("Вы забрали остаток.");
+                products.remove(nameOfProduct);
+            }
+        } else {
+            System.out.println("Такого продукта нет!");
+        }
+    }
+
     // 2 вывести на терминал какой продут закончится самым первым
+    public String endFirstProdukt() {
+        String nameProduktEnd = null;
+        int count = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Integer> food : products.entrySet()) {
+            String nameFoodEnd = food.getKey();
+            int valueFoodEnd = food.getValue();
+
+            if (valueFoodEnd < count) {
+                count = valueFoodEnd;
+                nameProduktEnd = nameFoodEnd;
+            }
+        }
+        return nameProduktEnd + " cкоро закончится";
+    }
+
     // 3 вывести всех продуктов в холодильнике
+    public int allValueProduks() {
 
-    // 4 вывести продукты и вес в остсортировонном формате по названию
-    // 5 вывести продуты и вес в остсортировонном формате по весу
+        int allValue = 0;
 
+        for (int value : products.values()) {
+            allValue += value;
+        }
+
+        return allValue;
+    }
+
+    // 4 - вывести продукты и вес в отсортированном формате по Названию
+    public void sortedByName() {
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(products.entrySet());
+        entryList.sort((entry2, entry1) -> entry2.getKey().compareTo(entry1.getKey()));
+        System.out.println("Сортировка по Названию: " + entryList);
+    }
+
+    // 5 - *** вывести продукты и вес в отсортированном формате по ВЕСУ(от большего к меньшему)
+    public void sortedByValue() {
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(products.entrySet());
+        entryList.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+        System.out.println("Сортировка по по Весу: " + entryList);
+
+    }
 }
--- src/lesson16/Main1.java
+++ src/lesson16/Main1.java
@@ -4,14 +4,26 @@ public class Main1 {
     public static void main(String[] args) {
         Freech freech = new Freech();
 
-        freech.addProduct("Яблоко",200);
-        freech.addProduct("Яблоко",400);
-        freech.addProduct("Груша",800);
-        freech.addProduct("Слива",1000);
+        freech.addProduct("яблоко",200);
+        freech.addProduct("яблоко",400);
+        freech.addProduct("груша",800);
+        freech.addProduct("слива",1000);
 
+        System.out.println("--- Перебор продуктов вариант 1 ---");
         freech.printAllProucts();
-        System.out.println("---------");
+        System.out.println("--- Перебор продуктов вариант 2 ---");
         freech.printAllProucts2();
+        System.out.println("--- Взять один продукт и вывести на консоль ---");
+        freech.getProdukt("Груша",300);
+        System.out.println("--- Продукт который скоро закончится ---");
+        System.out.println(freech.endFirstProdukt());
+        System.out.println("--- Вес всех продуктов ---");
+        System.out.println(freech.allValueProduks());
+        System.out.println("--- Сортировка по имени ---");
+        freech.sortedByName();
+        System.out.println("--- Сортировка по весу ---");
+        freech.sortedByValue();
+
 
     }
 }
